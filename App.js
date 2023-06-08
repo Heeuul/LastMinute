20cab4dc45c4f352d0fe67748585f409cb674673
Hans <aimanhans@yahoo.com>
2023-06-08 17:53:47 +0800

    080623 - 17:53 - Todo

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import StackNavigator from "./StackNavigator";
 
+// TODO: Press on logo clears all inputs: ref to inputs
+// TODO: MapScreen Inputs
+
 export default function App() {
   return (
     <Provider store={store}>
